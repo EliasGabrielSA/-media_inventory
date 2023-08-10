50d11d250a94873523ea45531990dde604cfdb3c
elias <eg.souzaandrade@gmail.com>
2023-08-10 14:39:46 -0300

    começando a implementar as funcoes

--- MediaInventory/src/RelacaoUserMidia.java
+++ MediaInventory/src/RelacaoUserMidia.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +10,41 @@
  *
  * @author ellia
  */
+
 class RelacaoUserMidia {
+    private Usuario usuario;
+    private ArrayList<Midia> midias;
+    private ArrayList<String> relacoes;
+    private String data_emprestimo;
+    private String data_devolucao;
+    
+    RelacaoUserMidia(Usuario usuario) {
+        this.usuario = usuario;
+        this.midias = new ArrayList<>();
+        this.relacoes = new ArrayList<>();
+        this.data_emprestimo = null;
+        this.data_devolucao = null;
+    }
+
+    public void setMidias(Midia midia) {
+        midias.add(midia);
+    }
+
+    public void setRelacoes(String relacao) {
+        relacoes.add(relacao);
+    }
+    
+    public void registrarEmprestimo(String data){
+        this.data_emprestimo = data;
+    }
+    
+    public void registrarDevolucao(String data) {
+        this.data_devolucao = data;
+    }
+    
+    
+    
+    
+    
     
 }
